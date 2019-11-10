d0baf08b97c464c3895b2bd7467f0171053497ca
Dan Lam <dmlam@ucsd.edu>
2019-11-10 11:16:57 -0800

    skeleton code

--- client/src/App/routes/Event.js
+++ client/src/App/routes/Event.js
@@ -1,13 +1,30 @@
-
-import React, { Component } from 'react';
-
+import React, {Component} from "react";
 
 class Event extends Component {
 
-    render(){
-        return(<p>Event. NOthing here yet.</p>);
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: []
+        }
     }
 
+    render() {
+        return (
+            <div>
+                <h1>{this.props.event.Eventname}</h1>
+                <div>
+                    <label>{this.props.event.Host}</label>
+                    <label>{this.props.event.Date}</label>
+                    <label>{this.props.event.Starttime}</label>
+                    <label>{this.props.event.Endtime}</label>
+                    <p>{this.props.event.Description}</p>
+                    <p>{this.props.event.Attendees}</p>
+                    <p>{this.props.event.tagIDs}</p>
+                    <label>{this.props.event.FlyerURL}</label>
+                </div>
+            </div>
+        );
+    }
 }
-
 export default Event;
