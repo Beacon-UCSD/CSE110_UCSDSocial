7861031526f3a74825b46a5948103e81b6d02ac7
Dan Lam <dmlam@ucsd.edu>
2019-11-11 12:41:47 -0800

    update to have unique parameter (Event ID) url and request event data

--- client/src/App/routes/Event.js
+++ client/src/App/routes/Event.js
@@ -5,24 +5,36 @@ class Event extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: []
+            event: {}
         }
+    }
+
+    componentDidMount = () =>{
+        fetch('/api/getEvent?EventID=' + this.props.match.params.EventID).then(res => res.json()).then(e => {
+            this.setState({
+                event: e
+            })
+        })
     }
 
     render() {
         return (
             <div>
-                <h1>{this.props.event.Eventname}</h1>
-                <div>
-                    <label>{this.props.event.Host}</label>
-                    <label>{this.props.event.Date}</label>
-                    <label>{this.props.event.Starttime}</label>
-                    <label>{this.props.event.Endtime}</label>
-                    <p>{this.props.event.Description}</p>
-                    <p>{this.props.event.Attendees}</p>
-                    <p>{this.props.event.tagIDs}</p>
-                    <label>{this.props.event.FlyerURL}</label>
-                </div>
+                <h1>Event Name: {this.state.event.Eventname}</h1>
+                <h2>
+                    Date: {this.state.event.Date}
+                    <br/> 
+                    Start Time: {this.state.event.Starttime}
+                    <br/> 
+                    End Time: {this.state.event.Endtime}
+                    <br/> 
+                </h2>
+                <h3>Tags: {this.state.event.tagID}</h3>
+                <h3>Host: {this.state.event.Host}</h3>
+                <h3>This is a {this.state.event.Private === "True" ? "Private": "Public"} event</h3>
+                <p>{this.state.event.Description}</p>
+                <h4>Attendees: {this.state.event.Attendees}</h4>
+
             </div>
         );
     }
