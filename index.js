da5c3ada4a41ca747bb510c722a8b6503fc91760
Dan Lam <dmlam@ucsd.edu>
2019-11-10 17:16:54 -0800

    Add TestEventCases

--- index.js
+++ index.js
@@ -3,13 +3,40 @@ const path = require('path');
 
 const app = express();
 
+const testEventList = [
+	{
+		tagID: "",
+		Eventname: "Event name 1",
+		Host: "dan",
+		Date: "11/11/11",
+		Starttime: "11:11",
+		Endtime: "7:30",
+		Private: "True",
+		Description: "This is a test event",
+		FlyerURL: "impath",
+		Ateendees: "tien, andrew"
+	},
+	{
+		tagID: "",
+		Eventname: "Event name 2",
+		Host: "dan",
+		Date: "11/11/11",
+		Starttime: "11:11",
+		Endtime: "7:30",
+		Private: "True",
+		Description: "This is a test event for event 2",
+		FlyerURL: "impath",
+		Ateendees: "tien, andrew"
+	}
+];
+
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
-	var list = ["Event1", "Event2", "Event3"];
-	res.json(list);
+	//var list = ["Event1", "Event2", "Event3"];
+	res.json(testEventList);
 	console.log('Sent list of items');
 });
 
