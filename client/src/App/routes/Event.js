2f74cb11f4f1a67804683e94de97b7a0653e5072
Dan Lam <dmlam@ucsd.edu>
2019-11-20 11:41:48 -0800

    CreateEvent func and date changes

--- client/src/App/routes/Event.js
+++ client/src/App/routes/Event.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import {Route} from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 class Event extends Component {
 
@@ -17,16 +19,17 @@ class Event extends Component {
         })
     }
 
+    handleUpdateEvent(){
+    }
+
     render() {
         return (
             <div>
                 <h1>Event Name: {this.state.event.Eventname}</h1>
                 <h2>
-                    Date: {this.state.event.Date}
+                    Start date: {this.state.event.Startdate}
                     <br/> 
-                    Start Time: {this.state.event.Starttime}
-                    <br/> 
-                    End Time: {this.state.event.Endtime}
+                    End date: {this.state.event.Enddate}
                     <br/> 
                 </h2>
                 <h3>Tags: {this.state.event.tagID}</h3>
@@ -34,7 +37,7 @@ class Event extends Component {
                 <h3>This is a {this.state.event.Private === "True" ? "Private": "Public"} event</h3>
                 <p>{this.state.event.Description}</p>
                 <h4>Attendees: {this.state.event.Attendees}</h4>
-
+                <button onClick={this.handleUpdateEvent}>Update Event</button>
             </div>
         );
     }
