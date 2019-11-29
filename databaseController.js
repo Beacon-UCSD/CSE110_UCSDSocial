83bdd354f48412a069c03a7a252bc70368441f30
cameronosmith <cos008@ucsd.edu>
2019-11-28 19:08:24 -0800

    DataBase Controller. Haven't implemented fetching users or tags but trivial to add with new interface.

---
+++ databaseController.js
@@ -0,0 +1,182 @@
+var mysql = require('mysql');                                                    
+
+//function to concatenate event object values to string for db query
+function eventToStr( eventObj ) {
+
+    //put event keys into array for ordered iterating
+    
+    valsStr      = ""
+    eventObjVals = [ eventObj.Eventname,
+                     eventObj.tagID,
+                     eventObj.Host,
+                     eventObj.Hostemail,
+                     eventObj.Startdate,
+                     eventObj.Enddate,
+                     eventObj.Private,
+                     eventObj.Description,
+                     eventObj.FlyerURL,
+                     eventObj.Attendees,
+                     eventObj.Forum
+                   ]
+
+    //concatenate event detail for aggregated query str
+    
+    for ( var idx = 0; idx < eventObjVals.length; idx++ ) {
+
+        if ( typeof(eventObjVals[idx] ) == "undefined" ) eventObjVals[idx] = ""
+
+        stripped = eventObjVals[idx].toString().replace(/[^a-z0-9@.]+/gi, " ")
+
+        valsStr += (idx==0 ? "" : ", ") + "\'" + stripped +"\'" ;    
+
+    }
+
+    return valsStr
+}
+
+//class for the databse interface
+
+class DbController {
+
+
+    //constructor for the db controller
+    //creates connection to the db
+    constructor() {
+
+        //db name
+        
+        this.db = "UCSDSocial";
+
+        //connect to the db
+
+        var connection = mysql.createConnection({                                        
+            host     : "beacon.cx82s6pkrof3.us-east-1.rds.amazonaws.com",                
+            user     : "root",                                                           
+            password : "Beacon110",                                                      
+            port     : "3306"                                                            
+        }); 
+
+        connection.connect(function(err) {
+
+            if(err) { 
+                console.error('Error connecting: ' + err.stack); 
+                return; 
+            }
+
+            console.log('Connected to db as id ' + connection.threadId);
+
+        }); 
+
+        //use proper db
+        
+        connection.query( "USE " + this.db + ";" );
+
+        //not sure why but storing in obj up there messes up async so did here
+        this.connection = connection;
+
+    }
+
+    //function to query for all events
+    //returns a promise to the query return obj
+    getAllEvents() {
+
+        var allEventsQuery = "SELECT * FROM Events;";
+
+        return this.makeQuery( allEventsQuery );
+
+    }
+
+    //function to store a new event to db
+    //currently overwrites any existing data so for update use this too
+    //eventObj holds the all the fields for an event
+    //returns a promise to the result of the insertion
+    storeEvent( eventObj ) {
+
+        //get the max event id first 
+        var maxIDQuery = "SELECT MAX(EventID) FROM Events;"
+        var tmp = this
+
+        return this.makeQuery( maxIDQuery ).then(function( maxID ) {
+    
+            var maxID = Number(maxID[0]['MAX(EventID)'])
+            var newID = ('000'+(Number(maxID) + 1)).substr(-3)
+            var storeEventQuery = "REPLACE INTO Events(EventID,Eventname,Tags,"+
+                "Hostname,Hostemail,Startdate,Enddate,Private,Description,"+
+                "FlyerURL,Attendees,Forum) VALUES('"+newID+"',"+
+                eventToStr( eventObj )+");";
+                        
+            return tmp.makeQuery( storeEventQuery );
+        })
+
+    }
+
+    //function to delete event from table
+    //eventID is the id for the event to delete
+    //returns a promise to the result of the insertion
+    deleteEvent( eventID ) {
+
+        var deleteEventQuery = "DELETE FROM Events where EventID="+eventID+";";
+
+        return this.makeQuery( deleteEventQuery );
+
+    }
+
+
+
+    //function to query for a specific event events
+    //eventID : the id of the queried event
+    //returns a promise to the query return obj
+    getEvent( eventID ) {
+
+        var specificEventQuery = 'SELECT * FROM Events where EventID =' + 
+                                                                eventID + ' ;';
+
+        return this.makeQuery( specificEventQuery );
+
+    }
+
+
+    //helper method to make a query since repetitive code
+    //returns promise to the query obj
+    makeQuery( query ) {
+
+        //need to store these here since promise doesn't have scope of this 
+        
+        var connection = this.connection;
+        var db = this.db;
+
+        //create promise and perform query 
+
+        return new Promise(function(resolve, reject) {
+
+            connection.query( query , db, function (error, results, fields) {
+
+                if ( error != null )
+                    console.log( error );                                                      
+
+                resolve( results )
+
+            });
+
+        });
+
+    }
+
+    //function to close the connection for this obj
+    closeConnection () {
+        try {
+
+            this.connection.end()
+            console.log("closed connection to db")
+
+        }
+        catch (err) {
+
+            console.log("caught error in trying to close db connection" + err); 
+
+        }
+    }
+
+}
+
+module.exports = DbController
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const jwt = require('express-jwt');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const DbController = require( './databaseController.js')
+
+//create our db instance
+const db = new DbController();
+
 // Gets an instance of the database controller.
 // Enlil: File not committed yet since still working on it.
 // const dbController = require('./DatabaseController');
@@ -11,52 +16,6 @@ const bodyParser = require('body-parser');
 const authenticator = require('./UserAuthenticator');
 
 
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-/*
- * FOR TESTING PURPOSES ONLY!
- * TODO REMOVE IN FUTURE
- */
-const testEventList = [
-	{
-		EventID: "0",
-		tagID: ["CSE110", "Project"],
-		Eventname: "CSE110 Project Meeting",
-		Host: "Dan Lam",
-		Startdate: (new Date(2019,0,12)).toString(),
-		Enddate: (new Date(2019,0,12,12)).toString(),
-		Private: true,
-		Description: "We will be working on the data",
-		FlyerURL: "",
-		Attendees: "tien, andrew, cameron"
-	},
-	{
-		EventID: "1",
-		tagID: ["Hangout", "Lunch", "Price Center"],
-		Eventname: "Team Lunch",
-		Host: "Gary",
-		Startdate: (new Date(2019,1,12)).toString(),
-		Enddate: (new Date(2019,1,12,12)).toString(),
-		Private: true,
-		Description: "Lunch meet-up at price center! Lets just chill and not talk about the project for once!",
-		FlyerURL: "",
-		Attendees: "tien, andrew, cameron, rujvi, dan"
-	},
-	{
-		EventID: "2",
-		tagID: ["Hangout", "Lunch", "Price Center"],
-		Eventname: "Lonely During Lunch?",
-		Host: "Teemo",
-		Startdate: (new Date(2019,0,13,3)).toString(),
-		Enddate: (new Date(2019,0,14,12)).toString(),
-		Private: false,
-		Description: "Lunch meet-up at price center! Lets just chill and we have free cookies!",
-		FlyerURL: "",
-		Attendees: ""
-	}
-];
 const testProfileList = [
     {
         UserID: "0",
@@ -78,8 +37,6 @@ const testProfileList = [
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-
-
 
 // Parse application/x-www-form-urlencoded
 // Not sure if required so left commented. If you have issues receiving form
@@ -123,9 +80,16 @@ app.use(jwt({secret: authenticator.JWT_SECRET}));
 
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
-	//var list = ["Event1", "Event2", "Event3"];
-	res.json(testEventList);
-	console.log('Sent list of items');
+
+    allEventsQuery = db.getAllEvents()
+
+    allEventsQuery.then(function( allEvents ){
+
+        res.json( allEvents );
+        console.log('Sent list of items');
+
+    })
+
 });
 
 app.get('/api/getUsers', (req,res) => {
@@ -133,22 +97,37 @@ app.get('/api/getUsers', (req,res) => {
 	console.log('Sent list of items');
 });
 
+///endpoint to get an event
 app.get('/api/getEvent', (req,res) => {
-	var eventid = req.query.EventID;
-	console.log('Attempt: Sent event ' + eventid);
-	var event = testEventList.filter(e => {
-		return e.EventID === eventid;
-	});
-	console.log('Result: ' + event[0].Eventname);
-	res.json(event[0]);
+
+    singleEventQuery = db.getEvent( req.query.EventID )
+
+    singleEventQuery.then(function( singleEvent ){
+
+        res.json( singleEvent[0] );
+
+        console.log( 'Sent event ' );
+
+    })
+
+});
+
+//endpoint to delete an event
+app.get('/api/deleteEvent', (req,res) => {
+
+    deleteEventQuery = db.deleteEvent( req.query.EventID )
+
+    deleteEventQuery.then(function( deleteEventResponse ){
+
+        console.log( 'Deleted event ' );
+
+    })
+
 });
 
 app.post('/api/storeEvent', function (req,res) {
-	console.log('Body of Request for storing: ');
-	console.log(req.body);
-	console.log('Attempt: Store new event: ' + req.body.Eventname);
-	var newEvent = {
-		EventID: testEventList.length.toString(10),
+
+	var eventObj = {
 		tagID: req.body.tagID,
 		Eventname: req.body.Eventname,
 		Host: req.body.Host,
@@ -159,16 +138,22 @@ app.post('/api/storeEvent', function (req,res) {
 		FlyerURL: req.FlyerURL,
 		Attendees: req.body.Attendees
 	}
-	testEventList.push(newEvent);
-	console.log('Number of events now: ' + testEventList.length);
-	console.log(testEventList);
+
+    storeEventQuery = db.storeEvent( eventObj )
+
+    storeEventQuery.then(function( storeEventResponse ){
+
+        console.log( 'Store event response: ' + storeEventResponse );
+
+    })
+
 });
 
 
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-	//res.sendFile(path.join(__dirname+'/client/build/index.html'));
+	res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
