7d943a898facd838a58dd904e6c14b80bfded4ec
arg049 <arg049@ucsd.edu>
2019-12-01 21:53:09 -0800

    Started working on attendees

--- client/src/App/routes/Event.js
+++ client/src/App/routes/Event.js
@@ -9,11 +9,17 @@ class Event extends Component {
     constructor(props) {
         super(props);
         this.userInfo = auth.getUserInfo();
+        console.log(this.userInfo);
 
         this.state = {
-            event: {}
-        }
+            event: {},
+            flag: true
+        };
+        console.log(this.state.event);
         this.handleUpdateEvent = this.handleUpdateEvent.bind(this);
+        this.handleJoinEvent = this.handleJoinEvent.bind(this);
+        this.joinEventStatus = this.joinEventStatus.bind(this);
+        console.log(this.state.event);
     }
 
     componentDidMount = () => {
@@ -23,8 +29,30 @@ class Event extends Component {
                 data.Startdate = new Date(data.Startdate).getTime();
                 data.Enddate = new Date(data.Enddate).getTime();
                 data.Tags = data.Tags.replace(/[\[\]"]+/gi, "").replace(/[,]+/gi, ", ");
-                this.setState({ event: data });
+
+                // TEMP: test
+                if (data.Attendees === "") {
+                    data.Attendees = [];
+                } else {
+                    data.Attendees = JSON.parse(data.Attendees);
+                }
+
+                this.setState({ event: data, flag: false });
+                console.log(this.state.event);
             });
+    }
+
+    handleJoinEvent() {
+        var attendees = {
+            userID: this.userInfo.id,
+            userName: this.userInfo.name
+        }
+
+        console.log(this.state.event.Attendees);
+        this.setState({
+            Attendees: this.state.event.Attendees.push(attendees)
+        });
+        console.log(this.state.event.Attendees);
     }
 
     handleUpdateEvent(){
@@ -44,9 +72,30 @@ class Event extends Component {
         })
     }
 
+    joinEventStatus() {
+        if (this.state.flag) {
+            return false;
+        }
+        console.log(this.state.event.Attendees);
+
+        var flag2 = false;
+        this.state.event.Attendees.forEach((function (item,index) {
+            if (this.userInfo.id === item.userID) {
+                console.log("true");
+                flag2 = true;
+                return flag2;
+            }
+        }).bind(this));
+        console.log("false");
+        return flag2;
+    }
+
     render() {
-        let showUpdate = this.userInfo.email == this.state.event.Hostemail ? 
+        let showUpdate = this.userInfo.email == this.state.event.Hostemail ?
             <button onClick={this.handleUpdateEvent}>Update Event</button> : null;
+        let showAttendance = this.joinEventStatus() === false ?
+            <button onClick={this.handleJoinEvent}>Join Event</button> : null;
+
         console.log(this.state.event);
         return (
             <div>
@@ -63,9 +112,10 @@ class Event extends Component {
                   <h3>Host: {this.state.event.Hostname}</h3>
                   <h3>This is a {this.state.event.Private === true ? "Private": "Public"} event</h3>
                   <p>{this.state.event.Description}</p>
-                  <h4>Attendees: {this.state.event.Attendees}</h4>
+                  <h4>Attendees: {JSON.stringify(this.state.event.Attendees)}</h4>
                   <img src={this.state.event.FlyerURL} height="150" width="150"/>
                   {showUpdate}
+                  {showAttendance}
               </div>
             </div>
         );
